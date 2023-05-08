fe80a684dd1ec38779fc8b561d65c8baf148d4d5
Neville Antony Jose <neville.jose@schrodinger.com>
2023-05-08 17:34:09 +0530

    Update example plugin

--- example/plugin-example/src/example/__init__.py
+++ example/plugin-example/src/example/__init__.py
@@ -6,19 +6,25 @@ class ExampleAssertionPlugin(BaseAssertionPlugin):
         print("Running validate_inputs from ExampleAssertionPlugin")
         print("Inputs:", inputs)
 
-        if "should_error" in inputs and inputs["should_error"]:
+        if (
+            "should_fail_validation" in inputs["metadata"]
+            and inputs["metadata"]["should_fail_validation"]
+        ):
             return "This is a validation error message."
 
         return None
 
-    def run_assertion(self,
-                      inputs: dict[str, object],
-                      state: dict[str, object]):
+    def run_assertion(
+        self, inputs: dict[str, object], state: dict[str, object]
+    ):
         print("Running run_assertion from ExampleAssertionPlugin")
         print("Inputs:", inputs)
         print("State:", state)
 
-        if "should_error" in inputs and inputs["should_error"]:
+        if (
+            "should_error" in inputs["metadata"]
+            and inputs["metadata"]["should_error"]
+        ):
             message = "This is an assertion error message."
             if "custom_message" in state and state["custom_message"]:
                 message += f" Plus here's a custom message: \
